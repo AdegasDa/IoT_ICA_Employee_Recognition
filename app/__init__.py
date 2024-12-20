b2635e74b1a662d653b699d90ba080cca4f7a1bc
AdegasDa <danieladegass@hotmail.com>
2024-12-20 12:34:24 +0000

    Add CRUD table for administration
    For both employee and users a table was created to perform crud operations

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
+from flask import Flask, url_for,render_template, request, redirect, session, jsonify
 
+from datetime import datetime
 import json
 import time
 from dotenv import load_dotenv
@@ -75,15 +76,191 @@ def attendance():
 @admin_is_required
 def admin():
     is_admin = str(session['user_id']) == config.get("GOOGLE_ADMIN_ID")
+    employees_data = my_db.get_all_employees()
+    if employees_data:
+        employees = employees_data
+    else:
+        employees = []
+    user = my_db.get_user_info(session['user_email'])
+    users = my_db.get_all_users()
+
+    return render_template("/admin.html", employees = employees, is_admin = is_admin, user = user, users = users)
+
+
+@app.route("/admin-users", methods = ["GET", "POST"])
+@login_is_required
+@admin_is_required
+def admin_users():
+    is_admin = str(session['user_id']) == config.get("GOOGLE_ADMIN_ID")
     users = my_db.get_all_users()
     user = my_db.get_user_info(session['user_email'])
 
-    return render_template("/admin.html", users = users, is_admin = is_admin, user = user)
+    return render_template("/admin_users.html", users = users, is_admin = is_admin, user = user)
 
 
-@app.route("/update/profile/<id>")
+@app.route("/update/profile/<id>", methods = ["POST", "GET"])
 def update_profile(id):
     pass
+
+
+@app.route("/add/user", methods=["POST"])
+@login_is_required
+@admin_is_required
+def add_user():
+    try:
+        # Retrieve form data
+        email = request.form.get("email")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm-password")
+        read_access = 1 if request.form.get("read_access") else 0
+        write_access = 1 if request.form.get("write_access") else 0
+
+        # Validate password confirmation
+        if password != confirm_password:
+            return "Passwords do not match", 400
+
+        # Prepare user data
+        user_data = {
+            "email": email,
+            "password": password,
+            "read_access": read_access,
+            "write_access": write_access
+        }
+
+        # Call the database helper to add the user
+        result = my_db.add_user(user_data)
+
+        # Check the result
+        if result["state"] == 1:
+            return redirect(url_for('admin_users'))
+        else:
+            print("Error Adding User:", result)
+            return "Error Adding User", 400
+
+    except Exception as e:
+        print(f"Exception in add_user: {str(e)}")
+        return "Internal Server Error", 500
+
+
+
+@app.route("/create/employee", methods=["POST"])
+@login_is_required
+@admin_is_required
+def create_employee():
+    user = my_db.get_user_info(request.form['user_email'])
+    day_of_week = []
+    for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+        if request.form.get(day):
+            day_of_week.append(day.capitalize())
+
+    # Collect form data
+    employee_data = {
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "hourly_rate": float(request.form['hourly_rate']),
+        "start_time": datetime.strptime(request.form['arrival_time'], "%H:%M").time(),
+        "end_time": datetime.strptime(request.form['departure_time'], "%H:%M").time(),
+        "day_of_week": " ".join(day_of_week),
+        "user_id": user.id
+    }
+
+    # Add employee using the database helper
+    result = my_db.add_employee(employee_data)
+
+    # Check result state and provide feedback
+    if result["state"] == 1:
+        return redirect(url_for('admin'))
+    else:
+        # Log the error and return a meaningful response
+        print("Error Editing Employee:", result)
+        return "Error Editing Employee", 400
+    
+
+@app.route("/delete/employee/<id>", methods=["POST", "GET"])
+@login_is_required
+@admin_is_required
+def delete_employee(id):
+    my_db.delete_employee(int(id))
+
+    return redirect(url_for('admin'))
+
+
+@app.route("/delete/user/<id>", methods=["POST", "GET"])
+@login_is_required
+@admin_is_required
+def delete_user(id):
+    my_db.delete_user(int(id))
+
+    return redirect(url_for('admin'))
+
+
+@app.route("/edit/user/<id>", methods=["POST"])
+@login_is_required
+@admin_is_required
+def edit_user(id):
+    try:
+        # Retrieve form data
+        email = request.form.get("email")
+        read_access = 1 if request.form.get("read_access") else 0
+        write_access = 1 if request.form.get("write_access") else 0
+
+        # Prepare user data
+        user_data = {
+            "id": int(id),
+            "email": email,
+            "read_access": read_access,
+            "write_access": write_access
+        }
+
+        # Call the database helper to update the user
+        result = my_db.update_user(id, user_data)
+
+        # Check the result
+        if result["state"] == 1:
+            return redirect(url_for('admin_users'))
+        else:
+            print("Error Editing User:", result)
+            return "Error Editing User", 400
+
+    except Exception as e:
+        print(f"Exception in edit_user: {str(e)}")
+        return "Internal Server Error", 500
+
+
+
+@app.route("/edit/employee", methods=["POST"])
+@login_is_required
+@admin_is_required
+def edit_employee():
+    user = my_db.get_user_info(request.form['edit_user_email'])
+
+    day_of_week = []
+    for day in ['edit_monday', 'edit_tuesday', 'edit_wednesday', 'edit_thursday', 'edit_friday', 'edit_saturday', 'edit_sunday']:
+        if request.form.get(day):
+            day = day.split("_")[1]
+            day_of_week.append(day.capitalize())
+
+    # Collect form data
+    employee_data = {
+        "first_name": request.form['edit_first_name'],
+        "last_name": request.form['edit_last_name'],
+        "hourly_rate": float(request.form['edit_hourly_rate']),
+        "start_time": datetime.strptime(request.form['edit_arrival_time'], "%H:%M:%S").time(),
+        "end_time": datetime.strptime(request.form['edit_departure_time'], "%H:%M:%S").time(),
+        "day_of_week": " ".join(day_of_week),
+        "user_id": user.id
+    }
+
+    # Add employee using the database helper
+    result = my_db.update_employee(employee_data)
+
+    # Check result state and provide feedback
+    if result["state"] == 1:
+        return redirect(url_for('admin'))
+    else:
+        # Log the error and return a meaningful response
+        print("Error Editing Employee:", result)
+        return "Error Editing Employee", 400
 
 
 @app.route("/logout", methods = ["POST", "GET"])
--- app/my_db.py
+++ app/my_db.py
@@ -1,12 +1,15 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, ForeignKey, Date, Time, Numeric
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.hybrid import hybrid_property
 import bcrypt
 
 db = SQLAlchemy()
 
 class User(db.Model):
     __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     email = db.Column(db.String(30))
     password = db.Column(db.String(255))
     token = db.Column(db.String(260))
@@ -24,6 +27,70 @@ class User(db.Model):
         self.write_access = write_access
         self.token = token
         self.login = login
+
+
+class Employees(db.Model):
+    __tablename__ = "employees"
+
+    employee_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    hourly_rate = db.Column(db.Numeric(10, 2), nullable=False)
+    day_of_week = db.Column(db.String(80), nullable=False)
+    start_time = db.Column(db.Time, nullable=False)
+    end_time = db.Column(db.Time, nullable=False)
+
+    # Relationship to Attendance
+    attendance = relationship('Attendance', back_populates='employee', cascade="all, delete-orphan")
+
+    def __init__(self, user_id, first_name, last_name, hourly_rate, day_of_week, start_time, end_time):
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.hourly_rate = hourly_rate
+        self.day_of_week = day_of_week
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+class Attendance(db.Model):
+    __tablename__ = 'attendance'
+
+    attendance_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.employee_id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    time_of_arrival = db.Column(db.Time, nullable=False)
+    time_of_departure = db.Column(db.Time, nullable=True)
+    hours_worked = db.Column(db.Numeric(5, 2), nullable=True)
+    total_cost = db.Column(db.Numeric(10, 2), nullable=True)
+
+    # Relationship to Employee
+    employee = relationship('Employees', back_populates='attendance')
+
+    def __init__(self, employee_id, date, time_of_arrival, time_of_departure, hours_worked, total_cost):
+        self.employee_id = employee_id
+        self.date = date
+        self.time_of_arrival = time_of_arrival
+        self.time_of_departure = time_of_departure
+        self.hours_worked = hours_worked
+        self.total_cost = total_cost
+
+
+    @hybrid_property
+    def calculated_hours_worked(self):
+        if self.time_of_arrival and self.time_of_departure:
+            arrival = datetime.combine(self.date, self.time_of_arrival)
+            departure = datetime.combine(self.date, self.time_of_departure)
+            delta = departure - arrival
+            return delta.total_seconds() / 3600 
+        return None
+
+    @hybrid_property
+    def calculated_total_cost(self):
+        if self.calculated_hours_worked and self.employee:
+            return self.calculated_hours_worked * self.employee.hourly_rate
+        return None
 
 
 def login(email, password):
@@ -98,3 +165,160 @@ def get_user_info(email):
         return res
 
     return None
+
+
+def get_all_employees():
+    employees = Employees.query.all()
+
+    if employees:
+        res = [employee for employee in employees]
+        res.sort(key=lambda employees: employees.hourly_rate)
+        return res
+    
+    return None
+
+
+def add_employee(employee):
+    try:
+        existing_employee = Employees.query.filter_by(user_id=employee['user_id']).first()
+        if existing_employee:
+            return {"state": 0, "message": "Employee already exists"}
+
+        new_employee = Employees(
+            user_id=employee['user_id'],
+            first_name=employee['first_name'],
+            last_name=employee['last_name'],
+            hourly_rate=employee['hourly_rate'],
+            day_of_week=employee['day_of_week'],
+            start_time=employee['start_time'],
+            end_time=employee['end_time'],
+        )
+
+        # Add to the database session
+        db.session.add(new_employee)
+        db.session.commit()
+        return {"state": 1, "message": "Employee added successfully"}
+    except Exception as e:
+        # Rollback in case of any errors
+        db.session.rollback()
+        return {"state": 0, "message": f"Error adding employee: {str(e)}"}
+    
+
+def update_employee(employee):
+    try:
+        existing_employee = Employees.query.filter_by(user_id=employee['user_id']).first()
+
+        if not existing_employee:
+            return {"state": 0, "message": "Employee does not exist"}
+
+        existing_employee.first_name = employee['first_name']
+        existing_employee.last_name = employee['last_name']
+        existing_employee.hourly_rate = employee['hourly_rate']
+        existing_employee.day_of_week = employee['day_of_week']
+        existing_employee.start_time = employee['start_time']
+        existing_employee.end_time = employee['end_time']
+
+        db.session.commit()
+
+        return {"state": 1, "message": "Employee updated successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"state": 0, "message": f"Error updating employee: {str(e)}"}
+
+
+def delete_employee(employee_id):
+    try:
+        existing_employee = Employees.query.filter_by(employee_id=employee_id).first()
+
+        if not existing_employee:
+            return {"state": 0, "message": "Employee does not exist"}
+
+        db.session.delete(existing_employee)
+
+        db.session.commit()
+
+        return {"state": 1, "message": "Employee deleted successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"state": 0, "message": f"Error deleting employee: {str(e)}"}
+
+
+def delete_user(id):
+    try:
+        existing_user = User.query.filter_by(id=id).first()
+
+        if not existing_user:
+            return {"state": 0, "message": "User does not exist"}
+
+        db.session.delete(existing_user)
+
+        db.session.commit()
+
+        return {"state": 1, "message": "User deleted successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"state": 0, "message": f"Error deleting user: {str(e)}"}
+    
+
+def add_user(user_data):
+    try:
+        # Check if the user already exists by email
+        existing_user = User.query.filter_by(email=user_data['email']).first()
+        if existing_user:
+            return {"state": 0, "message": "User already exists"}
+
+        # Hash the password
+        password_bytes = user_data['password'].encode('utf-8')
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+        # Create a new user instance
+        new_user = User(
+            email=user_data['email'],
+            password=hashed_password,
+            register_date=datetime.now(),
+            read_access=user_data['read_access'],
+            write_access=user_data['write_access'],
+            token="",
+            login=0  # Default to logged out
+        )
+
+        # Add and commit the new user
+        db.session.add(new_user)
+        db.session.commit()
+        return {"state": 1, "message": "User added successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"state": 0, "message": f"Error adding user: {str(e)}"}
+
+
+def update_user(user_id, user_data):
+    try:
+        # Find the user by ID
+        existing_user = User.query.filter_by(id=user_id).first()
+
+        if not existing_user:
+            return {"state": 0, "message": "User does not exist"}
+
+        # Update fields if provided
+        if 'email' in user_data:
+            existing_user.email = user_data['email']
+
+        if 'password' in user_data and user_data['password']:
+            password_bytes = user_data['password'].encode('utf-8')
+            salt = bcrypt.gensalt()
+            hashed_password = bcrypt.hashpw(password_bytes, salt)
+            existing_user.password = hashed_password
+
+        if 'read_access' in user_data:
+            existing_user.read_access = user_data['read_access']
+
+        if 'write_access' in user_data:
+            existing_user.write_access = user_data['write_access']
+
+        # Commit changes
+        db.session.commit()
+        return {"state": 1, "message": "User updated successfully"}
+    except Exception as e:
+        db.session.rollback()
+        return {"state": 0, "message": f"Error updating user: {str(e)}"}
